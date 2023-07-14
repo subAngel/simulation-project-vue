e52d7b782c093190ad2eb54a85931acb48e7e1ca
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-07-14 01:39:59 -0600

    Agregando un modal para dar informacion de la tabla

--- src/views/Datatable.vue
+++ src/views/Datatable.vue
@@ -8,6 +8,53 @@
 					<i class="fa-solid fa-chart-simple"></i>
 					Grafica
 				</router-link>
+				<button class="btn btn-info ml-9" onclick="my_modal_2.showModal()">
+					<i class="fa-solid fa-circle-info text-2xl"></i> Info
+				</button>
+				<dialog id="my_modal_2" class="modal">
+					<form method="dialog" class="modal-box w-4/5">
+						<h3 class="font-bold text-lg">Información!</h3>
+						<table class="table">
+							<tbody>
+								<!-- row 1 -->
+								<tr>
+									<th>PM2.5</th>
+									<td>
+										Material Particulado menor a 2.5 micrómetros
+									</td>
+								</tr>
+								<tr>
+									<th>TMCP</th>
+									<td>Tasa de mortalidad por cáncer de pulmón</td>
+								</tr>
+								<tr>
+									<th>RR (mínimo/máximo)</th>
+									<td>Riesgo Relativo</td>
+								</tr>
+								<tr>
+									<th>AF (mínimo/máximo)</th>
+									<td>Fracción Atribuible</td>
+								</tr>
+								<tr>
+									<th>TMCP 2.5 (mínimo/máximo)</th>
+									<td>
+										Estimación de muertes por cáncer de pulmón
+										por TM2.5
+									</td>
+								</tr>
+								<tr>
+									<th>(mínimo/máximo)</th>
+									<td>
+										Concentración contrafactual mínimo o máximo
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</form>
+					<form method="dialog" class="modal-backdrop">
+						<button>close</button>
+					</form>
+				</dialog>
 			</div>
 			<Table></Table>
 		</div>
