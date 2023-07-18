cc86ecfc1ff8bf4e32cdc4064d2b56f4d929d166
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-07-18 14:41:37 -0600

    creando el componente para el drawer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,28 +19,11 @@
 				</router-link>
 			</div>
 		</div>
-		<div class="absolute bottom-8 right-8">
-			<div class="drawer">
-				<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-				<div class="drawer-content">
-					<!-- Page content here -->
-					<label for="my-drawer" class="btn btn-primary drawer-button"
-						>Open drawer</label
-					>
-				</div>
-				<div class="drawer-side">
-					<label for="my-drawer" class="drawer-overlay"></label>
-					<div class="menu">
-						<div class="w-1/2 h-1/2 bg-terciary">
-							<h1 class="title">Hola mundo</h1>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Drawer />
 	</div>
 </template>
 
 <script setup>
 import Preview from "../components/PreviewModal.vue";
+import Drawer from "../components/Drawer.vue";
 </script>
