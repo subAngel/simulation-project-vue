5ba81e85c657f70243620f2552816e61d75cb8ed
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-07-20 16:44:05 -0600

    Cambiando BASE en vite.config.js

--- src/router/router.js
+++ src/router/router.js
@@ -37,10 +37,10 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(),
+	// history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
 	base: import.meta.env.BASE_URL,
-	// history: createWebHistory(),
 	// routes,
 });
 
